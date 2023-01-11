540ffe6e98808be29ac43a725a280f32302205a9
Mike Starr <imhs@protonmail.com>
2023-01-11 10:19:28 -0500

    transpile design-system

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,6 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-// const withTM = require("next-transpile-modules")(["@raidguild/design-system"]);
+const withTM = require("next-transpile-modules")(["@raidguild/design-system"]);
 
 // module.exports = withTM(nextConfig); // withPlugins([withNx, withTM], nextConfig);
