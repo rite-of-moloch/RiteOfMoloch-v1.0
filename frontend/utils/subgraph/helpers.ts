ec7d89b100ab694764d46aaa467a5023181f7e02
Shayan <91401566+sha256yan@users.noreply.github.com>
2023-02-14 20:49:51 +0800

    Update subgraph endpoint

--- frontend/utils/subgraph/helpers.ts
+++ frontend/utils/subgraph/helpers.ts
@@ -1,6 +1,6 @@
 export const performQuery = async (query: string) => {
   const subgraphEndpoint =
-    "https://api.studio.thegraph.com/query/40280/rom-test/v0.0.3";
+    "https://api.studio.thegraph.com/query/40280/rom-test/v0.0.5";
 
   const res = await fetch(subgraphEndpoint, {
     method: "POST",
