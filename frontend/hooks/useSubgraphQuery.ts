315977b2ee43751b14c94ff40b765bd9422e99e7
Mike Starr <imhs@protonmail.com>
2023-02-22 09:45:38 -0500

    fix env files

--- frontend/hooks/useSubgraphQuery.ts
+++ frontend/hooks/useSubgraphQuery.ts
@@ -5,17 +5,6 @@ export const useSubgraphQuery = (query: string) => {
   const subgraphEndpoint =
     "https://api.studio.thegraph.com/query/40280/rom-test/v0.0.15";
 
-  // const fetchSubgraphData = async () => {
-  //   await axios
-  //     .post(subgraphEndpoint, {
-  //       query,
-  //     })
-  //     .then((res) => {
-  //       console.log(res);
-  //       return res;
-  //     });
-  // };
-
   const { data, isLoading } = useQuery([query], () =>
     axios.post(subgraphEndpoint, { query })
   );
@@ -23,8 +12,6 @@ export const useSubgraphQuery = (query: string) => {
   if (isLoading) {
     console.log("query is loading...");
   }
-
-  // console.log("data", data);
 
   return { data, isLoading };
 };
