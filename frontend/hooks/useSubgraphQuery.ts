006a71d8e30b8384d7574d51e77aae33f515a6e8
Shayan <91401566+sha256yan@users.noreply.github.com>
2023-03-09 21:22:07 +0800

    new endpoint url version

--- frontend/hooks/useSubgraphQuery.ts
+++ frontend/hooks/useSubgraphQuery.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const useSubgraphQuery = (query: string) => {
   const subgraphEndpoint =
-    "https://api.studio.thegraph.com/query/40280/rom-test/v0.0.15";
+    "https://api.studio.thegraph.com/query/40280/rom-test/v0.0.17";
 
   const { data, isLoading } = useQuery([query], () =>
     axios.post(subgraphEndpoint, { query })
