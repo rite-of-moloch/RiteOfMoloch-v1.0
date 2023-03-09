7e9cc0208d6bc51ae38f5cce746e3675666627e0
Mike Starr <imhs@protonmail.com>
2023-03-08 21:40:14 -0500

    applied stash

--- frontend/components/AddAdminModal.tsx
+++ frontend/components/AddAdminModal.tsx
@@ -12,12 +12,9 @@ import {
   VStack,
   Input,
   HStack,
-  Text,
 } from "@raidguild/design-system";
 import { Modal } from "@chakra-ui/modal";
 import { FieldValues, useForm } from "react-hook-form";
-import { GrAdd } from "react-icons/gr";
-import { FaMinus } from "react-icons/fa";
 
 interface AddAdminModalProps {
   address: string | undefined;
@@ -29,50 +26,39 @@ interface AddAdminModalProps {
  */
 const AddAdminModal: React.FC<AddAdminModalProps> = ({ address }) => {
   const [editAdmin, setEditAdmin] = useState(false);
-  const [hideAdmin1, setHideAdmin1] = useState(false);
-  const [hideAdmin2, setHideAdmin2] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const localForm = useForm<FieldValues>();
   const { getValues, watch, setValue } = localForm;
   const values = getValues();
-  console.log(values);
 
-  const admin1 = "0x000000000000001";
-  const admin2 = "0x000000000000002";
-  const admins = [admin1];
+  // TODO: replace adminPlaceholders with values from subgraph
+
+  const admin1Placeholder = "0x000000000000001";
+  const admin2Placeholder = "0x000000000000002";
 
   const handleEdit = () => {
     setEditAdmin(true);
-    console.log(editAdmin);
   };
 
-  const handleSave = () => {
+  const handleSaveEdit = () => {
     // submit transaction to HATS contract
-    console.log("handleSave");
+    setValue("admin1", values?.admin1);
+    setValue("admin2", values?.admin2);
+    watch();
     setEditAdmin(false);
+    onClose();
   };
 
   const handleCancelEdit = () => {
-    console.log("cancel");
+    setValue("admin1", admin1Placeholder);
+    setValue("admin2", admin2Placeholder);
+    watch();
     setEditAdmin(false);
+    onClose();
   };
 
-  const handleAddAdmin1 = () => {
-    setHideAdmin1(!hideAdmin1);
-  };
-
-  const handleAddAdmin2 = () => {
-    setHideAdmin2(!hideAdmin2);
-  };
-
-  const handleDeleteAdmin1 = () => {
-    setValue("admin1", "");
-  };
-
-  const handleDeleteAdmin2 = () => {
-    setValue("admin2", "");
-  };
+  console.log(editAdmin);
 
   return (
     <>
@@ -82,75 +68,55 @@ const AddAdminModal: React.FC<AddAdminModalProps> = ({ address }) => {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay onClick={onClose} />
         <ModalContent>
-          <ModalHeader>Add admins</ModalHeader>
+          <ModalHeader>Cohort Administrators</ModalHeader>
           <ModalCloseButton />
           <ModalBody mx="-1.5em">
             <VStack spacing={3} w={["full", "80%"]} m="auto">
-              {/* start admin1 */}
-              <HStack alignContent="center" alignItems="end" w="full">
-                <Button
-                  size="sm"
-                  mb={1}
-                  color="black"
-                  onClick={() => handleAddAdmin1}
-                >
-                  {hideAdmin1 ? <GrAdd size="1rem" /> : <FaMinus size="1rem" />}
-                </Button>
-                {!admins[0] ? (
-                  <Input
-                    label=""
-                    name="admin2"
-                    placeholder="Enter address"
-                    type="text"
-                    localForm={localForm}
-                    display={hideAdmin1 ? "none" : ""}
-                  />
-                ) : (
-                  <Box border="1px solid red" pb={2} pt={1} px={2} rounded="md">
-                    <Text>{admin1}</Text>
-                  </Box>
-                )}
-              </HStack>
-              {/* start admin2 */}
-              <HStack alignContent="center" alignItems="end" w="full">
-                <Button
-                  size="sm"
-                  mb={1}
-                  color="black"
-                  onClick={handleAddAdmin2}
-                >
-                  {hideAdmin2 ? <GrAdd size="1rem" /> : <FaMinus size="1rem" />}
-                </Button>
-                {!admins[1] ? (
-                  <Input
-                    label=""
-                    name="admin2"
-                    placeholder="Enter address"
-                    type="text"
-                    localForm={localForm}
-                    display={hideAdmin2 ? "none" : ""}
-                  />
-                ) : (
-                  <Box border="1px solid red" pb={2} pt={1} px={2} rounded="md">
-                    <Text>{admin2}</Text>
-                  </Box>
-                )}
-              </HStack>
+              <Input
+                label="Admin 1"
+                name="admin1"
+                placeholder="Enter address"
+                type="text"
+                defaultValue={admin1Placeholder}
+                localForm={localForm}
+                autoComplete="false"
+                isDisabled={!editAdmin}
+              />
+              <Input
+                label="Admin 2"
+                name="admin2"
+                placeholder="Enter address"
+                type="text"
+                defaultValue={admin2Placeholder}
+                autoComplete="false"
+                localForm={localForm}
+                isDisabled={!editAdmin}
+              />
             </VStack>
           </ModalBody>
           <ModalFooter>
             <HStack>
-              <Box w="fit">
-                <Button onClick={editAdmin ? handleSave : handleEdit} size="md">
-                  {editAdmin ? "Save changes" : "edit"}
-                </Button>
+              <Box>
+                {editAdmin ? (
+                  <Button onClick={handleSaveEdit} size="md">
+                    Save changes
+                  </Button>
+                ) : (
+                  <Button onClick={handleEdit} size="md">
+                    Edit
+                  </Button>
+                )}
               </Box>
-              <Box
-                onClick={handleCancelEdit}
-                w="fit"
-                display={editAdmin ? "" : "none"}
-              >
-                <Button size="md">Cancel</Button>
+              <Box onClick={handleCancelEdit}>
+                {editAdmin ? (
+                  <Button onClick={handleCancelEdit} size="md">
+                    Cancel
+                  </Button>
+                ) : (
+                  <Button onClick={() => onClose()} size="md">
+                    Close
+                  </Button>
+                )}
               </Box>
             </HStack>
           </ModalFooter>
