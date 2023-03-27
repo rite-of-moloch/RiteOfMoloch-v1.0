b02224caab121d42a22ffa93b54e481c56727661
Shayan S <91401566+sha256yan@users.noreply.github.com>
2023-03-27 22:02:08 +1100

    COMMENTED OUT BuiltByRaidGuild in Footer
    RGDS bug

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import { BuiltByRaidGuild } from "@raidguild/design-system";
+//import { BuiltByRaidGuild } from "@raidguild/design-system";
 
 export const Footer = () => {
   return (
     <footer style={{ marginBottom: "1rem", marginTop: "1rem" }}>
-      <BuiltByRaidGuild />
+      {/*<BuiltByRaidGuild />*/}
     </footer>
   );
 };
