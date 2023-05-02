e616b89dffb77139c319faad36e019be6be1a40a
Hunter King <hking6776@gmail.com>
2023-05-02 11:57:04 -0500

    uncommment addr

--- frontend/utils/constants.ts
+++ frontend/utils/constants.ts
@@ -9,6 +9,7 @@ export const CONTRACT_ADDRESSES: { [key: string]: { [key: string]: string } } =
     100: {
       erc20TokenAddress: "0x18E9262e68Cc6c6004dB93105cc7c001BB103e49",
       // riteOfMolochFactoryAddress: "0xe9c1090d498da72242a0834c984493a5a7c7ca57",
+      riteOfMolochFactoryAddress: "0x67A9929392CfA00f8D15ee5c46ca662B174B8dd0",
     },
   };
 
