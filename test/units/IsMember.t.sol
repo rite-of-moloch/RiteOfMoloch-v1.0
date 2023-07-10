b5061029ecd18fcdad4a31307cf3f098d7bae269
bitbeckers <code@bitbeckers.com>
2023-07-10 13:35:18 +0200

    feat(test): isMember call

---
+++ test/units/IsMember.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "test/TestHelper.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @dev see note on TestHelper
+ */
+contract IsMemberTest is TestHelper {
+    function setUp() public override {
+        TestHelper.setUp();
+    }
+    /**
+     * TESTS
+     */
+
+    function testIsMember(address initiate) public {
+        vm.assume(initiate != address(0));
+
+        assertFalse(riteOfMoloch.isMember(initiate));
+        vm.mockCall(dao, abi.encodeWithSelector(IBaal.sharesToken.selector), abi.encode(sharesToken));
+        vm.mockCall(
+            address(sharesToken), abi.encodeWithSelector(ERC20.balanceOf.selector, initiate), abi.encode(minStake)
+        );
+
+        assertTrue(riteOfMoloch.isMember(initiate));
+
+        vm.clearMockedCalls();
+    }
+}
